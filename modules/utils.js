09177f52ae9225f5866587ade44d4abd4aa824f2
Auric Vente <mprops@protonmail.com>
2023-03-28 04:36:10 -0600

    Mods

--- modules/utils.js
+++ modules/utils.js
@@ -6,12 +6,18 @@ module.exports = function (App) {
   App.update_config = function (key, value) {
     try {
       App.config[key] = value
-      let p = "./config.json"
+      let p = App.get_config_path()
       let s = JSON.stringify(App.config, null, 2)
       App.fs.writeFileSync(p, s)
     }
     catch (err) {
+      console.log(err)
       console.error("Error updating the config file")
     }
   } 
+  
+  App.get_config_path = function () {
+    let p = App.path.resolve(App.path.dirname(__filename), "../")
+    return App.path.join(p, "config.json")
+  }
 }
