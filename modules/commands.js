5a6dbdec713ed4b50d2c7d257b5d3689d0947afe
Auric Vente <mprops@protonmail.com>
2023-04-04 09:45:29 -0600

    Mods

--- modules/commands.js
+++ modules/commands.js
@@ -40,7 +40,7 @@ module.exports = function (App) {
   }
 
   App.change_rules = function (to, rules) {
-    rules = rules.substring(0, App.config.max_rules)
+    rules = rules.substring(0, App.config.max_rules).trim()
     App.update_config("rules", rules)
     App.cmd_show(to, "rules")
   }
