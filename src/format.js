6ee0cba760e16a7bc586627239fc39983eb554d6
Auric Vente <mprops@protonmail.com>
2025-01-19 14:15:07 -0600

    Mods

--- src/format.js
+++ src/format.js
@@ -28,10 +28,14 @@ module.exports = (App) => {
 
       for (let match of matches) {
         if (full) {
-          text = text.replace(match[0], func(match[0]))
+          if (match[0].trim()) {
+            text = text.replace(match[0], func(match[0]))
+          }
         }
         else {
-          text = text.replace(match[0], func(match[1]))
+          if (match[1].trim()) {
+            text = text.replace(match[0], func(match[1]))
+          }
         }
       }
     }
